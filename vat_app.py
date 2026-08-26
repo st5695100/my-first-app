f43d93caa91ae00269093e3a35b5043901eb3552
st5695100 <56951@yupparaj.ac.th>
2026-08-26 11:30:32 +0700

    Create vat_app.py

---
+++ vat_app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+st.title("🛒แอปพลิเคชั่นคำนวณราคาสินค้ารวม VAT 7%")
+price = st.number_input("กรอกราคาสินค้า (บาท):", value=0.0)
+vat = price * 0.07
+net_price = price - vat
+st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท")
+st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
+st.divider()
+st.write("นางสาวพิมพ์นิภา คิดการงาน เลขที่ 41  ม.4/17")
